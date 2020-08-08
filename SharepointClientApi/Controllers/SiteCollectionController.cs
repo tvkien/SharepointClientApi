bd26b8c42e6f3b0732598eb587eade9b871866f4
TRUONG KIEN <TRUONG KIEN@DESKTOP-OJ1MFRG>
2020-08-08 11:52:13 +0700

    Add new controller SiteCollectionController and implement CreateComunicateSiteAsync action

---
+++ SharepointClientApi/Controllers/SiteCollectionController.cs
@@ -0,0 +1,39 @@
+﻿using SharepointClientApi.Domains.Abstractions;
+using SharepointClientApi.Models;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SharepointClientApi.Controllers
+{
+    public class SiteCollectionController : ApiController
+    {
+        private readonly ISiteCreation siteCreation;
+
+        public SiteCollectionController(ISiteCreation siteCreation)
+        {
+            this.siteCreation = siteCreation;
+        }
+
+        [Route("api/spo/CreateComunicateSite")]
+        [HttpPost]
+        public async Task<IHttpActionResult> CreateComunicateSiteAsync([FromBody] CreateComunicateSiteRequest request)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var siteUrl = await siteCreation.CreateCommunicationSiteAsync(request.CommunicationSiteRequest());
+
+                return Ok(siteUrl);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ SharepointClientApi/Models/CreateComunicateSiteRequest.cs
@@ -0,0 +1,27 @@
+﻿using SharepointClientApi.Domains;
+using System.ComponentModel.DataAnnotations;
+
+namespace SharepointClientApi.Models
+{
+    public class CreateComunicateSiteRequest
+    {
+        [Required]
+        public string Alias { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public Language Language { get; set; }
+
+        public CommunicationSiteRequest CommunicationSiteRequest()
+            => new CommunicationSiteRequest
+            {
+                Alias = Alias,
+                Title = Title,
+                Description = Description,
+                Language = Language
+            };
+    }
+}
